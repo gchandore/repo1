c5b89e487c913cc51eb35334fbe03a0e4bb4a3a0
gchandore <gchandore@yahoo.com>
2019-03-25 21:15:15 +0530

    First commit for JavaFile

---
+++ GitTest.java
@@ -0,0 +1,16 @@
+package SeleniumSeries;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","E:\\SeleniumJars\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://ydtwebstaging.com/");
+
+	}
+
+}
